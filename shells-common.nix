9bf5563cf96c848f7ea09e2b153e9afd320f9dee
Gábor Kerekes <krks.gbr@gmail.com>
2020-03-22 12:32:21 +0100

    Update shell aliases

--- shells-common.nix
+++ shells-common.nix
@@ -6,9 +6,10 @@
     conf = "emacs ~/.config/nixpkgs & disown";
     ls = "ls --color=auto";
     grep = "grep --color=always";
-    halup = "ssh -t gbr@krks.info '~/halu.space/exec_pms.sh'";
-    halucp = "rsync -avzP --remove-source-files --omit-dir-times ~/halu.stage/ gbr@krks.info:";
+    halup = "ssh -t gbr@ssh.krks.info '~/halu.space/exec_pms.sh'";
+    halucp = "rsync -avzP --remove-source-files --omit-dir-times ~/halu.stage/ gbr@ssh.krks.info:";
     play = "chromium --kiosk http://localhost:8080";
+    clip = "xclip -selection clipboard";
   };
   sessionVariables = {
     HOME_CONFIG_DIR = "$HOME/.config/nixpkgs";
