8ce1de56dd85d7d2756a1fd78210bebc2c902673
Gábor Kerekes <krks.gbr@gmail.com>
2020-05-21 19:45:49 +0200

    Enable avahi

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -5,4 +5,12 @@
   networking.networkmanager.insertNameservers = [
     "8.8.8.8"
   ];
+  networking.hostName = "krks";
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish.enable = true;
+    publish.addresses = true;
+    publish.workstation = true;
+  };
 }
