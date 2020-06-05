9c7f278a94ba9fa75a72a6b79c365371fdd79efe
Gábor Kerekes <krks.gbr@gmail.com>
2020-06-06 00:53:35 +0200

    Remove most of networking.nix
    The configuration messed with the windows server.

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,18 +1,7 @@
 { pkgs, ... }:
 {
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.br-b09373e26b5d.useDHCP = true;
-  networking.interfaces.br-ba41fd83df2f.useDHCP = true;
-  networking.interfaces.docker0.useDHCP = true;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp0s20f3.useDHCP = true;
-
   networking.networkmanager.enable = true;
   networking.networkmanager.insertNameservers = [
     "8.8.8.8"
   ];
-  networking.hostName = "krks";
 }
