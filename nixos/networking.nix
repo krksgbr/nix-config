b3fe70ca152ce9ca0a6206334280b9bc90995e1b
Gábor Kerekes <krks.gbr@gmail.com>
2020-06-05 15:52:57 +0200

    Remove avahi

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -16,11 +16,4 @@
     "8.8.8.8"
   ];
   networking.hostName = "krks";
-  services.avahi = {
-    enable = true;
-    nssmdns = true;
-    publish.enable = true;
-    publish.addresses = true;
-    publish.workstation = true;
-  };
 }
