ef9a1cb40b8df94d64150bc7d088ac2e40bb6299
Gábor Kerekes <krks.gbr@gmail.com>
2021-06-10 13:16:17 +0200

    Enable json in emacs doom

--- nixos/dev/editors/doom/init.el
+++ nixos/dev/editors/doom/init.el
@@ -148,6 +148,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
+       json
        yaml              ; JSON, but readable
 
        :email
