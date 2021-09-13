e0108115caeef65513b0c7916911124765271975
Gábor Kerekes <krks.gbr@gmail.com>
2021-09-13 14:22:24 +0200

    Setup static IP

--- nixos/system/networking.nix
+++ nixos/system/networking.nix
@@ -4,4 +4,12 @@
   networking.networkmanager.insertNameservers = [
     "8.8.8.8"
   ];
+
+  networking.interfaces.wlp0s20f3.useDHCP = false;
+  networking.interfaces.wlp0s20f3.ipv4.addresses = [{
+    address = "192.168.1.199";
+    prefixLength = 24;
+  }];
+
+  networking.defaultGateway = "192.168.1.1";
 }
