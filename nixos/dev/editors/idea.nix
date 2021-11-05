3e7747dfe64862c9a757f6aefce9cbc57929879a
Gábor Kerekes <krks.gbr@gmail.com>
2021-11-05 12:55:22 +0100

    Use latest intellij

--- nixos/dev/editors/idea.nix
+++ nixos/dev/editors/idea.nix
@@ -1,15 +1,15 @@
 { config, lib, pkgs, ... }:
 let
   # cli launcher
-  idea = pkgs.writeShellScriptBin "idea" ''
+  cliLauncher = pkgs.writeShellScriptBin "idea" ''
     idea-community $1 > /dev/null 2>&1 & disown;
   '';
 in
 {
   hm = {
     home.packages = with pkgs; [
-      idea
-      legacy.idea.idea-community
+      cliLauncher
+      idea.idea-community
     ];
     home.file.".ideavimrc".source = ./ideavimrc;
   };
