7533230dd21215a8bd871955e5934f5e32ab4975
Gábor Kerekes <krks.gbr@gmail.com>
2021-11-13 18:50:34 +0100

    Disable ocaml mode in emacs
    Breaks installation procedure

--- nixos/dev/editors/doom/init.el
+++ nixos/dev/editors/doom/init.el
@@ -124,7 +124,7 @@
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
-       (ocaml +lsp)             ; an objective camel
+       ;;(ocaml +lsp)             ; an objective camel
        (org              ; organize your plain life in plain text
         +dragndrop       ; file drag & drop support
         +ipython         ; ipython support for babel
