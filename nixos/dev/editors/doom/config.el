5853eb5648313ccb60250905a6c4425b36513325
Gábor Kerekes <krks.gbr@gmail.com>
2021-11-28 11:43:21 +0100

    Remove dead doom config

--- nixos/dev/editors/doom/config.el
+++ nixos/dev/editors/doom/config.el
@@ -4,12 +4,6 @@
 
 ;; hooks
 
-;; these interfere with auto-save
-(remove-hook! emacs-lisp-mode #'(doom|enable-delete-trailing-whitespace auto-compile-on-save-mode))
-;; to be able to save silently
-(remove-hook 'after-save-hook #'+evil|save-buffer)
-(remove-hook '+write-mode-hook #'mixed-pitch-mode)
-
 (add-hook 'prog-mode-hook #'rainbow-mode)
 
 (load! "+bindings")
@@ -17,12 +11,6 @@
       #'(visual-fill-column-mode
          visual-line-mode
          +write|init-org-mode))
-
-;;(after! yasnippet
-;;(setq g/snippets-dir (concat +private-config-path "/snippets"))
-;;(setq yas--default-user-snippets-dir g/snippets-dir)
-;;(add-to-list 'yas-snippet-dirs g/snippets-dir))
-
 
 (when doom-debug-mode
   (add-hook 'window-setup-hook (lambda () (switch-to-buffer "*Messages*"))))
