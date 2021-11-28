931333e1cf97a7eaf4f76296d38dfd49f62437b5
Gábor Kerekes <krks.gbr@gmail.com>
2021-11-28 11:55:13 +0100

    Pin nixpkgs pkgSet and add unpinned/unstable pkgSet

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "nixos";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?rev=c5ed8beb478a8ca035f033f659b60c89500a3034";
     nixpkgs-legacy.url = "github:nixos/nixpkgs/nixos-20.09";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager.url = "github:nix-community/home-manager";
     emacs-overlay.url = "github:nix-community/emacs-overlay?rev=209234286d219ca9990707a1f2377e731048bbd2";
@@ -29,6 +31,7 @@
                 {
                   stable = pkgSet inputs.nixpkgs-stable;
                   legacy = pkgSet inputs.nixpkgs-legacy;
+                  unstable = pkgSet inputs.nixpkgs-unstable;
                 })
               inputs.emacs-overlay.overlay
             ];
