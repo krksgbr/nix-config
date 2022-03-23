b83f61b6cb6e5f3253e690815b1282cd61a240ac
Gábor Kerekes <krks.gbr@gmail.com>
2022-03-23 15:41:30 +0100

    Move starship to its own file

--- nixos/dev/shell/zsh/default.nix
+++ nixos/dev/shell/zsh/default.nix
@@ -6,39 +6,9 @@
       ./plugins/zsh-completions.nix
       ./plugins/zsh-history-substring-search.nix
       ./plugins/zsh-vi-mode.nix
+      ./starship.nix
     ];
 
-    programs.starship = {
-      enable = true;
-      enableZshIntegration = true;
-      settings = {
-        format = "$directory$git_branch$git_commit$git_status$git_state$nix_shell\n$character";
-        git_branch = {
-          format = "[| $branch]($style)";
-          style = "bright-green";
-        };
-        git_status = {
-          format = "$ahead_behind$modified$untracked$staged";
-          ahead = "[ ⇡](green)";
-          behind = "[ ⇣](green)";
-          diverged = "[ ⇕](yellow)";
-          modified = "[*](bright-green)";
-          untracked = "[*](red)";
-          staged = "[+](green)";
-        };
-        git_commit = {
-          style = "yellow";
-          format = " [\\($hash\\)]($style) [\\($tag\\)]($style)";
-        };
-        git_state = {
-          format = " \([$state( $progress_current/$progress_total)]($style)\)";
-        };
-        nix_shell = {
-          format = " [|](bright-green) [❄]($style)";
-          style = "bright-green";
-        };
-      };
-    };
 
     programs.zsh = {
       enable = true;
