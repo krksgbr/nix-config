eec36edece0aea7a23aba57c5175a237718171ad
Gábor Kerekes <krks.gbr@gmail.com>
2022-03-23 15:41:52 +0100

    Use another plugin for zsh vim mode
    The one by jeffreytse had a problem with multiline prompts inside tmux:
    on mode change, terminal lines above were eaten up. It was annoying.
    This new plugin has the same issue, but there is an easy fix.

--- nixos/dev/shell/zsh/plugins/zsh-vi-mode.nix
+++ nixos/dev/shell/zsh/plugins/zsh-vi-mode.nix
@@ -3,29 +3,15 @@
   programs.zsh = {
     plugins = [
       {
-        name = "zsh-vi-mode";
+        name = "zsh-vim-mode";
         src = fetchGit {
-          url = "https://github.com/jeffreytse/zsh-vi-mode.git";
-          rev = "0c6f34a84fa4d5781c36135b3a7fddb426473c1b";
+          url = "https://github.com/softmoth/zsh-vim-mode.git";
+          rev = "1f9953b7d6f2f0a8d2cb8e8977baa48278a31eab";
         };
       }
     ];
     sessionVariables = {
-      ZVM_VI_EDITOR = "vim";
-      ZVM_LINE_INIT_MODE = "$ZVM_MODE_INSERT";
-      ZVM_VI_HIGHLIGHT_BACKGROUND = "black";
+      MODE_INDICATOR = "";
     };
-
-    initExtra = ''
-      function zvm_after_lazy_keybindings() {
-         if [[ -n $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND ]]; then
-             zvm_bindkey vicmd 'k' history-substring-search-up
-             zvm_bindkey vicmd 'j' history-substring-search-down
-         fi
-
-         zvm_define_widget command-history
-         zvm_bindkey vicmd '/' command-history
-      }
-    '';
   };
 }
