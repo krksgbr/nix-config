ac879ce660e2637bbf93081a807af3833f8adeb6
Gábor Kerekes <krks.gbr@gmail.com>
2022-03-23 15:48:51 +0100

    Update some flake inputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "nixos";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?rev=c5ed8beb478a8ca035f033f659b60c89500a3034";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-legacy.url = "github:nixos/nixpkgs/nixos-20.09";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager.url = "github:nix-community/home-manager";
-    emacs-overlay.url = "github:nix-community/emacs-overlay?rev=209234286d219ca9990707a1f2377e731048bbd2";
+    emacs-overlay.url = "github:nix-community/emacs-overlay?rev=c939047c31f01535942826d831987154157c8d8d";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, ... }: {
@@ -29,7 +28,6 @@
             [
               (_: _:
                 {
-                  stable = pkgSet inputs.nixpkgs-stable;
                   legacy = pkgSet inputs.nixpkgs-legacy;
                   unstable = pkgSet inputs.nixpkgs-unstable;
                 })
